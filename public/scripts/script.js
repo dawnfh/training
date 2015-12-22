af5203ebc40a59bb5a0e64d1a254a49f28e6c993
Michael <Mikeks81@gmail.com>
2015-12-22 01:29:57 -0500

    restructured the about page to include animation and images.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,7 +7,26 @@ $(document).ready(function(){
   $("#name").slideDown( 1000 );
 
   $("#name").animate({left: '700px'});
-    
+  
+  $("#about_container").mouseenter(
+  	function(){
+  		$(this).animate({width: '60%'},1000),
+  		$("#about_image_container").animate({
+  			width: '39%'
+  		},1000),
+  		$(".entire_team p").show();
+  	});
+
+   $("#about_container").mouseleave(
+  	function(){
+  		$(this).animate({width: '20%'},1000),
+  		$("#about_image_container").animate({
+  			width: '59%'
+  		},1000),
+  		$(".entire_team p").hide();
+  	});
+
+
 });
    
 
